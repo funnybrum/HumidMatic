1c312304a73975e13d1815f78434840fa5d69b18
funnybrum <funnybrum@gmail.com>
2020-02-20 08:37:16 +0200

    Trying to get more precise estimate for the humidification cycle.

--- src/Humidifier.cpp
+++ src/Humidifier.cpp
@@ -15,10 +15,6 @@ void Humidifier::begin() {
     digitalWrite(_pwmPin, HIGH);
 
     pinMode(_waterOKPin, INPUT);
-
-    analogWriteFreq(100);
-    analogWriteRange(100);
-    _dutyCycle = 100;
 }
 
 void Humidifier::loop() {
@@ -27,55 +23,91 @@ void Humidifier::loop() {
         digitalWrite(_fanPin, LOW);
         digitalWrite(_pwmPin, HIGH);
         logger.log("Stopping due to low water level.");
-        _onHumidity = -1;
         _onTimestamp = 0;
+        _cycleDuration = 0;
         return;
     }
 
-    if (influxClient.isDataAvailable()) {
-        float humidity = influxClient.getQueryResult();
-        influxClient.purgeData();
+    float humidity = influxClient.getQueryResult();
+    bool isDataAvailable = influxClient.isDataAvailable();
+    if (isDataAvailable) {
         logger.log("Humidity is %.1f", humidity);
-
-        if (humidity < settings.getSettings()->hm.targetHumidityLow) {
-            digitalWrite(_fanPin, HIGH);
-            // the _pwmPin is inversed. 0/LOW means ON, 100/HIGH means off.
-            analogWrite(_pwmPin, 100 - _dutyCycle);
-            logger.log("Humidity lower than specified, turning on with duty cycle = %d%%", _dutyCycle);
-
-            _onHumidity = humidity;
-            _onTimestamp = millis();
-        }
-        
-        if (humidity >= (0.98 * settings.getSettings()->hm.targetHumidityHigh)) {
-            // We are pretty close to the targe humidity.
-            digitalWrite(_fanPin, LOW);
-            digitalWrite(_pwmPin, HIGH);
-            logger.log("Humidity target reached, turning off.");
-
-            if (_onTimestamp != 0) {
-                // Recalc the required duty cycle to get the target humidity in a number of whole
-                // intervals.
-
-                // 1) Find out the humidity raise in 1 interval with 100% duty cycle.
-                float cycleMillis = settings.getSettings()->ifxSettings.queryInterval * 1000.0;
-                uint8_t cycles = round((millis() - _onTimestamp) / cycleMillis);
-                float oneCycleHumidityRaise = (humidity - _onHumidity) * (100.0 / _dutyCycle) / cycles;
-                logger.log("Humidity got from %.1f to %.1f in %d cycles with dc=%d%%", _onHumidity, humidity, cycles, _dutyCycle);
-                logger.log("Maximum humidity raise on 1 cycle is %.1f", oneCycleHumidityRaise);
-  
-                // 2) Get the number of required cycles to reach the target humidity.
-                float requiredCycles = (settings.getSettings()->hm.targetHumidityHigh - settings.getSettings()->hm.targetHumidityLow) / oneCycleHumidityRaise;
-                logger.log("Required cycles to reach target humidity are %.1f", requiredCycles);
-
-                // 3) Get the required duty cycle to reach the target humidity in a number of whole intervals.
-                _dutyCycle = 100.0 * requiredCycles / ceil(requiredCycles);  // I.e. 100 * 2.7 / 3 = 90% duty cycle
-                logger.log("Duty cycle for not overshooting target humidity is %d", _dutyCycle);
-
-                // 4) Clean up.
-                _onTimestamp = 0;
-                _onHumidity = -1;
-            }
-        }
+        influxClient.purgeData();
     }
+
+    bool humidityLow = isDataAvailable &&
+        (humidity < settings.getSettings()->hm.targetHumidityLow);
+
+    bool humidityHigh = isDataAvailable &&
+        (humidity >= settings.getSettings()->hm.targetHumidityHigh);
+
+    bool humidificationCycleCompleted = _cycleDuration > 0 &&
+         (millis() - _onTimestamp > _cycleDuration);
+
+    if (humidityLow) {
+        digitalWrite(_fanPin, HIGH);
+        digitalWrite(_pwmPin, LOW);
+        logger.log("Last humidity is %.1f, turning on.", humidity);
+        _calcNextCycleDuration(humidity, settings.getSettings()->hm.targetHumidityLow);
+        _onTimestamp = millis();
+    }
+    
+    if (humidityHigh) {
+        digitalWrite(_fanPin, LOW);
+        digitalWrite(_pwmPin, HIGH);
+        logger.log("Last humidity is %.1f, turning off", humidity);
+        _cycleDuration = 0;
+        _offTimestamp = millis();
+    }
+    
+    if (humidificationCycleCompleted) {
+        digitalWrite(_fanPin, LOW);
+        digitalWrite(_pwmPin, HIGH);
+        logger.log("Humidification cycle completed, turning off");
+        _cycleDuration = 0;
+        _offTimestamp = millis();
+    }
+}
+
+void Humidifier::_calcNextCycleDuration(float currentHumidity, float targetHumidity) {
+    if (_onTimestamp == 0) {
+        // There was no previous humidification cycle. Without such the next duty cycle
+        // duration can not be calculated;
+        return;
+    }
+
+    wifi.connect();
+
+    // 1) Find out the humidity at the start of the previous cycle.
+    bool result = influxClient.query((millis() - _onTimestamp) / 60000);
+    if (!result) {
+        logger.log("Failed to get previous start cycle humidity.");
+        wifi.disconnect();
+        return;
+    }
+    float startCycleHumidity = influxClient.getQueryResult();
+    influxClient.purgeData();
+
+    // 2) Find out the humidity at the end of the previous cycle.
+    result = influxClient.query((millis() - _offTimestamp) / 60000);
+    if (!result) {
+        logger.log("Failed to get previous end cycle humidity.");
+        wifi.disconnect();
+        return;
+    }
+    float endCycleHumidity = influxClient.getQueryResult();
+    influxClient.purgeData();
+
+    // 3) Based on the previous cycle duration and the humidity raise in it - calculate the
+    // required cycle length to reach the upper humidity threshold.
+    float lastHumidityRaise = endCycleHumidity - startCycleHumidity;
+    unsigned long lastCycleDuration = _offTimestamp - _onTimestamp;
+    float targetHumidityRaise = targetHumidity - currentHumidity;
+    _cycleDuration = (targetHumidityRaise/lastHumidityRaise) * lastCycleDuration;
+
+    wifi.disconnect();
+    logger.log("Prev start/end humidity was %.1f / %.1f", startCycleHumidity, endCycleHumidity);
+    logger.log("Last cycle duration was %d seconds", lastCycleDuration/1000);
+    logger.log("Current humidity is %.1f, target humidity raise is %.1f", currentHumidity, targetHumidityRaise);
+    logger.log("Next cycle duration is %d seconds", _cycleDuration / 1000);
 }
--- src/Humidifier.h
+++ src/Humidifier.h
@@ -8,13 +8,19 @@ class Humidifier {
         void begin();
         void loop();
     private:
+        void _calcNextCycleDuration(float currentHumidity, float targetHumidity);
+        
         uint8_t _fanPin;
         uint8_t _pwmPin;
         uint8_t _waterOKPin;
-        // The two below are used to calculate the required duty cycle in order
-        // to keep the humidity in the target range.
-        float _onHumidity;
+        // Keep a timestamp when humidifier was turned on and off last time. These will be used
+        // before the next humidifier ON cycle to calculate the required duration to get to the
+        // target humidity levels.
         unsigned long _onTimestamp;
+        unsigned long _offTimestamp;
 
-        uint8_t _dutyCycle;
+        // The following will controll how long the humidifier should work. If it is 0 - this is
+        // not in effect because this is the first humidification cycle. If > 0 the humidification
+        // will be stopped when the _onTimestamp + _cycleDuration millis are reached.
+        unsigned long _cycleDuration;
 };
